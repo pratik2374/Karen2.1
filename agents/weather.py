5fbbf209dd9a3959a5c0317cd4ef68c58a4db7a1
PRATIK GOND <itamazes@gmail.com>
2025-06-12 21:55:50 +0530

    weather and tinuUrl

---
+++ agents/weather.py
@@ -0,0 +1,26 @@
+from crewai import Agent, Task, Crew
+from langchain_openai import ChatOpenAI
+from composio_crewai import ComposioToolSet, Action, App
+composio_toolset = ComposioToolSet(api_key="8qr3cp4a09u4zfsgd7tuzm")
+tools = composio_toolset.get_tools(actions=['WEATHERMAP_WEATHER'])
+
+# Define agent
+crewai_agent = Agent(
+    role="Sample Agent",
+    goal="""You are an AI agent that is responsible for taking actions based on the tools you have""",
+    backstory=(
+        "You are AI agent that is responsible for taking actions based on the tools you have"
+    ),
+    verbose=True,
+    tools=tools,
+    llm=ChatOpenAI(),
+)
+task = Task(
+    description="your task description here",
+    agent=crewai_agent,
+    expected_output=""
+)
+my_crew = Crew(agents=[crewai_agent], tasks=[task])
+
+result = my_crew.kickoff()
+print(result)
